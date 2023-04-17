70d46953bff42c717f0eda41cabeeb358c0f4328
Bhaarat Krishnan <88877925+bhaaratkrishnan@users.noreply.github.com>
2023-04-17 22:00:25 +0530

    docs done naive solution

--- top-50-array-questions/move_all_negative_elements_to_end_of_array.py
+++ top-50-array-questions/move_all_negative_elements_to_end_of_array.py
@@ -3,15 +3,19 @@ class Solution:
         # Your code goes here
         lst1 = []
         lst2 = []
+        #we are seperating the positive and negative elements in 2 diffrent arrays
         for i in arr:
             if i >= 0:
                 lst1.append(i)
             else:
                 lst2.append(i)
         j = 0
+        #then we placing in order in the given array
+        #starting from positive element array
         for i in lst1:
             arr[j] = i
             j += 1
+        #then negative element array
         for i in lst2:
             arr[j] = i
             j += 1
